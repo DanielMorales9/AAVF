27709280864c901ca6c075c5500a18e02b4ea8cb
FedericoMartinelli <mar_tinelli@hotmail.it>
2016-12-18 21:36:50 +0100

    entropy

---
+++ TexturalProperties/entropy.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+class Entropy:
+
+    def __init__(self, image):
+        self.image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    def get_entropy(self):
+        hist = cv2.calcHist(self.image, [0], None, [256], [0,256])
+        hist /= np.sum(hist)
+        log = cv2.log(hist)
+        return -1 * np.sum(log * hist)
